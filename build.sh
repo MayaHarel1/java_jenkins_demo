eea8aa023a2a2b3c81ae8baae62b63c62c970bc2
MayaHarel1 <168677917+MayaHarel1@users.noreply.github.com>
2025-08-11 18:25:54 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Parameters from Jenkins
+NAME=${1:-World}
+ENVIRONMENT=${2:-DEV}
+
+# Compile Java code
+javac src/HelloWorld.java
+
+# Run Java app
+java -cp src HelloWorld "$NAME" "$ENVIRONMENT"
